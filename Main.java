3d8e61bcc8869cd41c7e310fecedbd9051f1ec68
ujwal <ujwal16042002@gmail.com>
2025-05-14 22:46:40 +0530

    added print

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello Java from Jenkins");
         System.out.println(new Date());
+        System.out.println("Hello");
     }
 }
