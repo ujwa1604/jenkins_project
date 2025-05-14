abfaa89cd8f16681b45d3a5958d0aa57d1a0230f
ujwal <ujwal16042002@gmail.com>
2025-05-14 21:36:22 +0530

    inital commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello Java from Jenkins");
+        System.out.println(new Date());
+    }
+}
